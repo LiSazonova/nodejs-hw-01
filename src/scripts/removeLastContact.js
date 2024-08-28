ac2ed95ff44b89aa3cd089ad97b8991f83f22d39
Lili Sazonova <lilissazonova@gmail.com>
2024-08-28 15:58:51 +0300

    add removeLast

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        const contacts = JSON.parse(data);
+        contacts.pop();
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts));
+        console.log('Last contact remove successfull!');
+    } catch (error) {
+        console.log('Failed to delete contact from the file:', error);
+    }
+};
 
 removeLastContact();
