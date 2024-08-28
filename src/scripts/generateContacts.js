97a6fe5c8c16542f7e60803efca6642c81fa2187
Lili Sazonova <lilissazonova@gmail.com>
2024-08-28 14:23:07 +0300

    add contacts.js, generateContacts.js

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        const contacts = JSON.parse(data);
+        for (let i = 0; i < number; i += 1) {
+            contacts.push(createFakeContact());
+        }
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts));
+        console.log('File created successfull!');
+    } catch (error) {
+        console.log('Failed to write contacts to the file:', error);
+    }
+};
 
 generateContacts(5);
