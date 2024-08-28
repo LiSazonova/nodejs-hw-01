381e953fcd16db3238360ed11f20e5da42df1849
Lili Sazonova <lilissazonova@gmail.com>
2024-08-28 15:55:50 +0300

    add removeAll

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    contacts.length = 0;
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts));
+    console.log('File created successfull!');
+};
 
 removeAllContacts();
